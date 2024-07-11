532971fd7c67a3cd3b30e1ae265859451de7c872
Ammar Aburuman <A.aburuman@protonmail.com>
2024-07-11 16:18:34 +0300

    add landing page with options and css styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { Login } from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Routes>
+          <Route path="/" exact element= {<Login/>}></Route>   {/*Login Page */}
+          <Route></Route>             {/*Seller Page */}
+          <Route></Route>             {/*Buyer Page */}
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css"
+
+
+export const Login = () => {
+    return (
+        <div className="container">
+            <h1>So, We selling or buying?</h1>
+            <form>
+            <button className="Buyer">Buying</button>
+            <button className="Seller">Selling</button>
+            </form>
+        </div>
+    )
+}
