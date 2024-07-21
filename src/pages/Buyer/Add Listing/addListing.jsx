44d4dc19987095b640942a75f5efc482570c6150
Ammar Aburuman <A.aburuman@protonmail.com>
2024-07-21 15:36:07 +0300

    Add fully functional list item page

--- src/pages/Buyer/Add Listing/addListing.jsx
+++ src/pages/Buyer/Add Listing/addListing.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom"
 import { addListingtoFirestore } from "../../../Firebase/firebase-actions";
+import { Grid,Header,Form, Segment, Divider, Label, TextArea, Button, Icon } from "semantic-ui-react";
 
 
 export const AddListing = () => {
@@ -28,25 +29,36 @@ export const AddListing = () => {
     }
 
     return (
-        <div>
-            <div>
-                <form className="lisiting-form" onSubmit={handle_submit}>
-                    <label>Item Name:</label>
-                    <input type="text" required placeholder="Item's name..." value={name} onChange={(e)=>{setName(e.target.value)}}/>
-                    <br />
+        <Grid textAlign="center" style = {{height: "70vh"}} verticalAlign="middle">
+            <Grid.Column style= {{maxWidth:1000}}>
+                <Header as="h1" color="blue" textAlign="center">
+                    Add your item to the marketplace
+                </Header>
+                <Form size="large">
+                    <Grid Grid columns={2} relaxed='very'>
+                    <Grid.Column>
+                    <Segment raised>
+                        <Form.Input  placeholder = "Item Name" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+                        <Form.Input labelPosition="right" type="text" placeholder="Item Price" value={price} onChange={(e)=>{setPrice(e.target.value)}}>
+                            <Label basic>$</Label>
+                            <input />
+                            <Label>.00</Label>
+                        </Form.Input>
+                        <TextArea placeholder='Describe the item you are trying to sell...' style={{marginBottom: "20px"}}/>
+                        <Button color="red" onClick={()=>navigate("/buyer")}>
+                            <Icon name="chevron left"/>Back</Button>
+                        <Button color="blue" type="submit" onClick={handle_submit}>List Item</Button>
+                    </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        Hello
+                    </Grid.Column>
+                    </Grid>
+                    <Divider vertical/>
+                </Form>
+            </Grid.Column>
 
-                    <label>Price:</label>
-                    <input type="text" required placeholder="Item's price..." value={price} onChange={(e)=>{setPrice(e.target.value)}}/>
-                    <br />
-
-                    <button type="submit" className="submit-btn">List for sale</button>
-
-
-                </form>
-
-            <Link  to ="/Buyer" className="back-btn">Back?</Link>
-            </div>
-        </div>
+        </Grid>
 
         
     )
